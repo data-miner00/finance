9db3a0192daf707dea3f9e03a339671ba871ab9d
Shaun Chong <data-miner00@users.noreply.github.com>
2026-05-12 20:50:39 +0800

    feat: Scaffold initial dashboard

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import SiteHeader from '$lib/components/site-header.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<Sidebar.Provider
+	style="--sidebar-width: calc(var(--spacing) * 72); --header-height: calc(var(--spacing) * 12);"
+>
+	<AppSidebar variant="inset" />
+	<Sidebar.Inset>
+		<SiteHeader />
+		<div class="flex flex-1 flex-col">
+			<div class="@container/main flex flex-1 flex-col gap-2">
+				{@render children()}
+			</div>
+		</div>
+	</Sidebar.Inset>
+</Sidebar.Provider>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1 class="text-3xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import ChartAreaInteractive from '$lib/components/chart-area-interactive.svelte';
+	import DataTable from '$lib/components/data-table.svelte';
+	import SectionCards from '$lib/components/section-cards.svelte';
+	import data from '$lib/components/data';
+</script>
+
+<div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+	<SectionCards />
+	<div class="px-4 lg:px-6">
+		<ChartAreaInteractive />
+	</div>
+	<DataTable {data} />
+</div>
