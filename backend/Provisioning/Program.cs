9627bf5e3eeae84ca8f2f5431124317ecd26c6cf
Shaun Chong <data-miner00@users.noreply.github.com>
2026-06-02 12:39:42 +0800

    chore: Add provisioning project

---
+++ backend/Provisioning/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Provisioning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
