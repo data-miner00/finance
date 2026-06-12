7c30d8b73ac2cda3eb46437161bac7adf263111c
Shaun Chong <data-miner00@users.noreply.github.com>
2026-06-12 18:23:58 +0800

    feat: Add error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import Button from '$lib/components/ui/button/button.svelte';
+
+	const emojis: Record<number, string> = {
+		404: '👻',
+		420: '🫠',
+		500: '💥'
+	};
+</script>
+
+<div class="flex h-full w-full items-center justify-center">
+	<div class="mx-auto my-auto w-fit text-center">
+		<h1 class="text-2xl font-bold">{page.status} {page.error?.message}</h1>
+		<span style="font-size: 10em">
+			{emojis[page.status] ?? emojis[500]}
+		</span>
+
+		<p>Looks like something is acting up. Apologies for the inconvenience!</p>
+
+		<div class="mt-4">
+			<Button onclick={() => history.back()}>Go back</Button>
+		</div>
+	</div>
+</div>
