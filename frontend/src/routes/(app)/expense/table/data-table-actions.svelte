4e221509836107a1c248c04f72d2cc693d982018
Shaun Chong <data-miner00@users.noreply.github.com>
2026-07-04 18:18:24 +0800

    feat: Add more fields for update expense

--- frontend/src/routes/(app)/expense/table/data-table-actions.svelte
+++ frontend/src/routes/(app)/expense/table/data-table-actions.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+	import { CheckIcon, ChevronsUpDownIcon } from '@lucide/svelte';
 	import EllipsisIcon from '@lucide/svelte/icons/ellipsis';
+	import { tick } from 'svelte';
 
 	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
 	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
+	import * as Command from '$lib/components/ui/command/index.js';
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
+	import * as Popover from '$lib/components/ui/popover/index.js';
 	import { deleteExpense, updateExpense } from '$lib/services';
 	import { appState } from '$lib/states.svelte';
+	import { cn } from '$lib/utils';
 
 	let { id }: { id: string } = $props();
 
@@ -18,6 +23,8 @@
 	let categoryName = $state<string>();
 	let amount = $state(0);
 	let actionedAt = $state('');
+	let location = $state<string>();
+	let description = $state<string>();
 
 	function openEditDialog() {
 		const item = appState.expenses.find((e) => e.id === id);
@@ -26,14 +33,27 @@
 		amount = item.amount;
 		categoryName = item.categoryName;
 		actionedAt = item.actionedAt;
+		location = item.location || undefined;
+		description = item.description || undefined;
 		isEditDialogOpen = true;
 	}
 
 	async function saveExpense(event: Event) {
 		event.preventDefault();
-		await updateExpense(id, { name, amount, categoryName, actionedAt });
+		await updateExpense(id, { name, amount, categoryName, actionedAt, location, description });
 		appState.expenses = appState.expenses.map((e) =>
-			e.id === id ? { ...e, name, amount, categoryName, updatedAt: new Date().toISOString() } : e
+			e.id === id
+				? {
+						...e,
+						name,
+						amount,
+						categoryName,
+						actionedAt,
+						location,
+						description,
+						updatedAt: new Date().toISOString()
+					}
+				: e
 		);
 		isEditDialogOpen = false;
 	}
@@ -42,6 +62,26 @@
 		await deleteExpense(id);
 		appState.expenses = appState.expenses.filter((e) => e.id !== id);
 		isDeleteDialogOpen = false;
+	}
+
+	let categoryComboOpen = $state(false);
+	let categoryTriggerRef = $state<HTMLButtonElement>(null!);
+
+	function closeAndFocusCategoryTrigger() {
+		categoryComboOpen = false;
+		tick().then(() => {
+			categoryTriggerRef.focus();
+		});
+	}
+
+	let locationComboOpen = $state(false);
+	let locationTriggerRef = $state<HTMLButtonElement>(null!);
+
+	function closeAndFocusLocationTrigger() {
+		locationComboOpen = false;
+		tick().then(() => {
+			locationTriggerRef.focus();
+		});
 	}
 </script>
 
@@ -82,6 +122,56 @@
 					<Input id="edit-name" name="name" placeholder="e.g., Texas Chicken" bind:value={name} />
 				</div>
 				<div class="grid gap-3">
+					<Label for="category-1">Category</Label>
+					<Popover.Root bind:open={categoryComboOpen}>
+						<Popover.Trigger bind:ref={categoryTriggerRef}>
+							{#snippet child({ props })}
+								<Button
+									{...props}
+									variant="outline"
+									class="justify-between"
+									role="combobox"
+									aria-expanded={categoryComboOpen}
+								>
+									{categoryName || 'Select or add a new category...'}
+									<ChevronsUpDownIcon class="opacity-50" />
+								</Button>
+							{/snippet}
+						</Popover.Trigger>
+						<Popover.Content class="p-0">
+							<Command.Root>
+								<Command.Input placeholder="Category..." bind:value={categoryName} />
+								<Command.List>
+									<Command.Empty>{categoryName}</Command.Empty>
+									<Command.Group value="frameworks">
+										{#each appState.categories as category (category)}
+											<Command.Item
+												value={category}
+												onSelect={() => {
+													categoryName = category;
+													closeAndFocusCategoryTrigger();
+												}}
+											>
+												<CheckIcon class={cn(categoryName !== category && 'text-transparent')} />
+												{category}
+											</Command.Item>
+										{/each}
+									</Command.Group>
+								</Command.List>
+							</Command.Root>
+						</Popover.Content>
+					</Popover.Root>
+				</div>
+				<div class="grid gap-3">
+					<Label for="description-1">Description</Label>
+					<Input
+						id="description-1"
+						name="description"
+						bind:value={description}
+						placeholder="e.g. It was delicious!"
+					/>
+				</div>
+				<div class="grid gap-3">
 					<Label for="edit-amount">Amount</Label>
 					<Input
 						id="edit-amount"
@@ -93,13 +183,47 @@
 					/>
 				</div>
 				<div class="grid gap-3">
-					<Label for="edit-category">Category</Label>
-					<Input
-						id="edit-category"
-						name="categoryName"
-						placeholder="e.g., Food, Transport"
-						bind:value={categoryName}
-					/>
+					<Label for="location-1">Location</Label>
+					<Popover.Root bind:open={locationComboOpen}>
+						<Popover.Trigger bind:ref={locationTriggerRef}>
+							{#snippet child({ props })}
+								<Button
+									{...props}
+									variant="outline"
+									class="justify-between"
+									role="combobox"
+									aria-expanded={locationComboOpen}
+								>
+									{location || 'Select or add a new location'}
+									<ChevronsUpDownIcon class="opacity-50" />
+								</Button>
+							{/snippet}
+						</Popover.Trigger>
+						<Popover.Content class="p-0">
+							<Command.Root>
+								<Command.Input placeholder="e.g. Melaka Supermarket" bind:value={location} />
+								<Command.List>
+									<Command.Empty>{location}</Command.Empty>
+									<Command.Group value="frameworks">
+										{#each appState.knownLocations as locationSuggestion (locationSuggestion)}
+											<Command.Item
+												value={locationSuggestion}
+												onSelect={() => {
+													location = locationSuggestion;
+													closeAndFocusLocationTrigger();
+												}}
+											>
+												<CheckIcon
+													class={cn(location !== locationSuggestion && 'text-transparent')}
+												/>
+												{locationSuggestion}
+											</Command.Item>
+										{/each}
+									</Command.Group>
+								</Command.List>
+							</Command.Root>
+						</Popover.Content>
+					</Popover.Root>
 				</div>
 			</div>
 			<Dialog.Footer>
